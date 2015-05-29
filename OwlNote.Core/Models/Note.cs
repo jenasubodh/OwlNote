4aae089d6aa1db4df9f7d8c6632220848a94c73a
Subodh Jena <jenasubodh@gmail.com>
2015-05-29 13:06:54 +0530

    Initial project commit (Project Structure)
    Initial project commit

---
+++ OwlNote.Core/Models/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SQLite;
+
+namespace OwlNote.Core.Models
+{
+	public class Note
+	{
+		[PrimaryKey, AutoIncrement]
+		public int ID { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public DateTime Date {get; set;}
+	}
+}
+
